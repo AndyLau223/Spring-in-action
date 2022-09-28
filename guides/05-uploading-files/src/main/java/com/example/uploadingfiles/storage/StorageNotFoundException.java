afbf0415199e7642c8ba40b2ba9508332c9c09dc
Andy <jiancong.liu@outlook.com>
2022-09-28 20:51:00 +0800

    Add File uploading

---
+++ guides/05-uploading-files/src/main/java/com/example/uploadingfiles/storage/StorageNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.uploadingfiles.storage;
+
+public class StorageNotFoundException extends StorageException {
+
+    public StorageNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ guides/05-uploading-files/src/test/java/com/example/uploadingfiles/UploadingFilesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.uploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+class UploadingFilesApplicationTests {
+
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
