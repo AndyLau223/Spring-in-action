7eedc305c7e83b184c4f80a25ac14c28eb0ac221
Andy <jiancong.liu@outlook.com>
2022-10-15 03:40:25 +0800

    Add Eureka

---
+++ guides2/23-crud-ui-with-vaadin/src/test/java/com/example/cruduiwithvaadin/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cruduiwithvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
